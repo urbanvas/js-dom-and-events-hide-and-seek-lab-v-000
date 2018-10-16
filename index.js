37ec80b30cfed43c614ed0e0ec4a4641fa214c80
Neil Seejoor <nvseejoor@gmail.com>
2018-10-16 20:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ function deepestChild() {
   return document.querySelector('#grand-node div div div div')
 }
 
-function increaseRankBy(n) {
 
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
